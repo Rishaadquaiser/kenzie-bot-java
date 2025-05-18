84cd71bfabb6d6e92a9263cf5974e292e00333fc
Rishaadquaiser <rishaad.quaiser@solace.com>
2025-05-18 12:18:22 -0400

    added application.properties and added db dependencies

--- src/main/java/com/kenzie/KenzieBotApplication.java
+++ src/main/java/com/kenzie/KenzieBotApplication.java
@@ -5,6 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KenzieBotApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("KenzieBot is running!");
     }
 }
---
+++ src/main/java/com/kenzie/db/DiscordFriend.java
@@ -0,0 +1,26 @@
+package com.kenzie.db;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "discord_friend")
+@Data
+public class DiscordFriend {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private String userId;
+
+    @Column(name = "friend_id", nullable = false)
+    private String friendId;
+
+    // Constructors, getters, and setters
+}
