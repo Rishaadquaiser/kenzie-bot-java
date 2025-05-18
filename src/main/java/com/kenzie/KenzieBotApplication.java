699cb05c8a1041f41927fd76a02491876d3fa4ca
Rishaadquaiser <rishaad.quaiser@solace.com>
2025-05-18 11:21:20 -0400

    created kenzie bot java project

---
+++ src/main/java/com/kenzie/KenzieBotApplication.java
@@ -0,0 +1,10 @@
+package com.kenzie;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenzieBotApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
