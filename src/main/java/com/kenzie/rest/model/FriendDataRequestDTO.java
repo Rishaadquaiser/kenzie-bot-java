c4d3d84c66f6a6f82fc544cf68b4f1577b3c2f65
Rishaadquaiser <rishaad.quaiser@solace.com>
2025-05-24 10:40:51 -0400

    FriendDataRequestDTO values are all notNull

--- src/main/java/com/kenzie/rest/model/FriendDataRequestDTO.java
+++ src/main/java/com/kenzie/rest/model/FriendDataRequestDTO.java
@@ -2,12 +2,17 @@ package com.kenzie.rest.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.jetbrains.annotations.NotNull;
 
 @Data
 @AllArgsConstructor
 public class FriendDataRequestDTO {
+    @NotNull
     private String name;
+    @NotNull
     private Long discordId;
+    @NotNull
     private String medsTime;
+    @NotNull
     private String timeZone;
 }
